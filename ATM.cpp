7f090c260b5d2054177f416b656e4b8fc27ee6ff
Praj41 <pcbhamare.41@gmail.com>
2020-08-29 14:47:06 +0530

    class Skeleton

---
+++ ATM.cpp
@@ -0,0 +1,38 @@
+//
+// Created by ADMIN on 06-07-2020.
+//
+
+#include "ATM.h"
+
+
+void ATM::showBal() {
+    std::cout << "Your Balance is:" << bal << std::endl;
+}
+
+void ATM::Deposit() {
+
+}
+
+void ATM::Withdraw() {
+
+}
+
+void ATM::changePIN() {
+
+}
+
+void ATM::displayAccInfo() {
+
+}
+
+void ATM::printTrans() {
+
+}
+
+void ATM::AccountLogin(std::string cname, int cPIN) {
+
+}
+
+ATM::ATM() {
+
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ATM.h"
+
+int main() {
+
+    ATM a;
+
+    a.showBal();
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
